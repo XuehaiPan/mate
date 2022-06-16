90c29d6a30dd8f9b8e3374a5b7c7d001f240300b
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-06-16 23:12:20 +0800

    chore: update imports

--- examples/hrl/__init__.py
+++ examples/hrl/__init__.py
@@ -1,6 +1,8 @@
 """Example of hierarchical agents for the Multi-Agent Tracking Environment."""
 
-from examples.hrl import ippo, iql, qmix, tarmac
+from examples.hrl import ippo, iql, mappo, qmix, tarmac
 from examples.hrl.ippo import HRLIPPOCameraAgent
 from examples.hrl.iql import HRLIQLCameraAgent
+from examples.hrl.mappo import HRLMAPPOCameraAgent
 from examples.hrl.qmix import HRLQMIXCameraAgent
+from examples.hrl.tarmac import HRLTarMACCameraAgent
