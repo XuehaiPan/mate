910d24e63e8a67706c962f0a64ee6ada37668641
Xuehai Pan <XuehaiPan@pku.edu.cn>
2022-08-15 23:58:54 +0800

    docs: update module docstring

--- examples/hrl/mappo/__init__.py
+++ examples/hrl/mappo/__init__.py
@@ -1,4 +1,4 @@
-"""Example of hierarchical agents for the Multi-Agent Tracking Environment."""
+"""Example of hierarchical MAPPO agents for the Multi-Agent Tracking Environment."""
 
 from examples.hrl.mappo import camera
 from examples.hrl.mappo.camera import HRLMAPPOCameraAgent
--- examples/hrl/tarmac/__init__.py
+++ examples/hrl/tarmac/__init__.py
@@ -1,4 +1,4 @@
-"""Example of TarMAC agents for the Multi-Agent Tracking Environment."""
+"""Example of hierarchical TarMAC agents for the Multi-Agent Tracking Environment."""
 
 from examples.hrl.tarmac import camera
 from examples.hrl.tarmac.camera import HRLTarMACCameraAgent
